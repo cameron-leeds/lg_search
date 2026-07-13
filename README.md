5598c1a7eec2102fcfde5f9209d974977b1b50d2
Cameron Leeds <leeds.cameron@gmail.com>
2026-07-13 08:51:54 -0400

    add py files and readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # lg_search
+
+i'm working on searching through these 3 sites
+
+ultimately rare carat is the winner, but want to get the best diamond by finding a matching LG number from the other 2
+
+Original search on rare carat - https://www.rarecarat.com/diamond-search/c1da7094-3c13-4a3f-8dc6-6ac2a9924047?shape=round,oval
+
+lookups on the other 2 sites
+
+https://aurelinne.com/pages/lab-grown - this one is trickier as it's all in the UI
+https://calaveranewyork.com/search?q=LG768694303&options%5Bprefix%5D=last - this one is easy as we can just replace the search
+
+Here is a scenario for rarecarat.com
+
+https://aurelinne.com/pages/lab-grown?id=b935142a-1806-4974-aaa7-30ff67d469dd&stoneType=diamond
+
+This diamond for $434
+
+https://www.rarecarat.com/diamond/147879647/2.21ct-f-vvs2-rare-carat-ideal-round-lab-grown-diamond?ref=back&ts=Search&cs=1
+
+Same diamond at rarecarat.com. open chat and give them both links and ask for them to price match.then give them the link for the setting you want.
+
+https://www.rarecarat.com/setting/170-Presentation-6-Prong-Solitaire-Engagement-Ring-Solitaire/1833983
+
+18k gold 6 prong setting for 502.
+
+Total ring will be 936 plus tax. Free shipping. The diamond alone was twice that price originally on rarecarat.com!
+
+Hope this helps.
---
+++ rare_carat/scrape.py
@@ -0,0 +1,81 @@
+from scrapy import Request, Spider
+from scrapers.items import HotelItemLoader, ReviewItemLoader
+from scrapers.utils import print_failure
+
+
+class RareCaratSpider(Spider):
+    """This class manages all the logic required for scraping the Rare Carat website.
+
+    Attributes:
+        name (str): The unique name of the spider.
+        start_url (str): Root of the website and first URL to scrape.
+        custom_settings (dict): Custom settings for the scraper
+    """
+
+    name = "rare_carat"
+
+    start_url = "https://www.rarecarat.com/diamond-search/02851e61-af1c-499f-a2a5-2391f95ff948?shape=round,oval"
+
+    custom_settings = {
+        "DEFAULT_REQUEST_HEADERS": {
+            "Connection": "close",
+        },
+
+        "DOWNLOADER_MIDDLEWARES": {
+            'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+            'scrapers.middlewares.retry.RetryMiddleware': 550,
+        },
+    }
+
+    def start_requests(self):
+        """This method start 10 separate sessions on the homepage, one per page."""
+        for page in range(1, 10):
+            yield Request(
+                url=self.start_url,
+                callback=self.parse,
+                errback=self.errback,
+                dont_filter=True,
+                meta=dict(
+                    page=page,
+                    cookiejar="jar%d" % page,
+                ),
+            )
+
+    def parse(self, response):
+        """After accessing the website's homepage, we retrieve the list of diamonds from page X."""
+        yield Request(
+            url=response.urljoin("diamonds?page=%d" % response.meta['page']),
+            callback=self.parse_listing,
+            errback=self.errback,
+            meta=response.meta,
+        )
+
+    def parse_listing(self, response):
+        """This method parses the list of diamonds from page X."""
+        for el in response.css('.diamond-link'):
+            yield response.follow(
+                url=el,
+                callback=self.parse_diamond,
+                errback=self.errback,
+                meta=response.meta,
+            )
+
+    def parse_diamond(self, response):
+        """This method parses diamond details such as name, email, and reviews."""
+        reviews = [self.get_review(review_el) for review_el in response.css('.diamond-review')]
+
+        diamond = DiamondItemLoader(response=response)
+        diamond.add_css('name', '.diamond-name::text')
+        diamond.add_css('email', '.diamond-email::text')
+        diamond.add_value('reviews', reviews)
+        return diamond.load_item()
+
+    def get_review(self, review_el):
+        """This method extracts rating from a review"""
+        review = ReviewItemLoader(selector=review_el)
+        review.add_css('rating', '.review-rating::text')
+        return review.load_item()
+
+    def errback(self, failure):
+        """This method handles and logs errors and is invoked with each request."""
+        print_failure(self.logger, failure)
